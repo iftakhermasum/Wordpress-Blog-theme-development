da73f1b2ac2e89351d4eac3ce928c3ac9cff98fb
iftakher <26634250+iftakhermasum@users.noreply.github.com>
2023-01-05 15:19:15 +0600

    Add files via upload

--- part 3/js/main.js
+++ part 3/js/main.js
@@ -1,1 +1,36 @@
+jQuery(document).ready(function(){
 
+// header search form
+    jQuery(".headrsrchwrp img").click(function(){
+        jQuery(".headrsrchwrp form").toggle();
+    });
+
+
+// owl carousel
+    jQuery('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        dots: true,
+        //autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
+
+//  menu toggle  
+    jQuery(".hambargerbox").click(function(){
+        jQuery(".hedrmenusec").toggle();
+    });
+
+
+
+  });
